f5de54c7dff59dd42cf76a5f254292d3df20095b
Cora-Jane Meade <cora-jane.meade@cognizant.com>
2026-06-11 15:55:43 +0100

    Generating presigned URL test

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,12 +1,13 @@
 import { health } from '#src/routes/health.js'
 import { locations } from '#src/routes/locations.js'
 import { getYears } from '../routes/years/get-years.js'
+import { generateDownloadLinks } from '../routes/years/generate-download-links.js'
 
 export const router = {
   plugin: {
     name: 'router',
     register: (server, _options) => {
-      server.route([health, ...locations, getYears])
+      server.route([health, ...locations, getYears, generateDownloadLinks])
     }
   }
 }
