bef5ece54c2c977705d5ee19926a1736df26fac4
Cora-Jane Meade <cora-jane.meade@cognizant.com>
2026-06-15 11:55:24 +0100

    Logs and functions to figure out where it is breaking

--- src/routes/years/get-years.js
+++ src/routes/years/get-years.js
@@ -5,7 +5,10 @@
 import { getYears as getYearsController } from '../../controllers/years-controller.js'
 import { createLogger } from '../../common/helpers/logging/logger.js' // Using existing logger setup to test
 import { config } from '#src/config.js'
-import { listBucketContents } from '../../services/s3-service.js' // Using existing S3 service to test
+import {
+  listBucketContents,
+  getDownloadLinksAndSaveToDB
+} from '../../services/s3-service.js' // Using existing S3 service to test
 import { statusCodes } from '../../common/constants/status-codes.js'
 
 const logger = createLogger('s3-service')
@@ -19,9 +22,9 @@ export const getYears = {
     notes: 'Returns a list of all years with their associated download links'
   },
   handler: async (request, h) => {
-    // S3 logic - check bucket and log file count
+    // TEMP S3 logic - check bucket and log file count
     const bucketName = config.get('s3.bucket')
-    const { prefix } = request.query
+    const prefix = 'reports/'
 
     try {
       const files = await listBucketContents(bucketName, prefix)
@@ -39,7 +42,28 @@ export const getYears = {
         `Failed to list S3 contents for ${bucketName}: ${error.message}. Continuing with DB years fetch.`
       )
     }
-
+    //end of TEMP S3 logic - it works
+    //generate download links for those that exist in S3 and assign to years in DB in chronigal order for now
+    try {
+      const presignedUrls = await getDownloadLinksAndSaveToDB(
+        request.db,
+        bucketName,
+        prefix
+      )
+      request.log(
+        `Successfully generated download links for whatever files are there and saved to DB but not to the correct years yet`
+      )
+      request.log(
+        ['info', 's3'],
+        `Presigned URLs: ${JSON.stringify(presignedUrls)}`
+      )
+      logger.info(`Presigned URLs: ${JSON.stringify(presignedUrls)}`)
+    } catch (error) {
+      request.log(['error', 's3'], error.message)
+      logger.error(
+        `Failed to generate download links and save to DB for ${bucketName}: ${error.message}. Continuing with DB years fetch.`
+      )
+    }
     // Get years from database
     try {
       const result = await getYearsController(request.db, request.logger)
